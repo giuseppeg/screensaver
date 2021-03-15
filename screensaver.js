d5225f6ae97bad27f2ad0a76744c1a9a6e0b572d
Giuseppe <giuseppeg@users.noreply.github.com>
2021-03-15 22:33:50 +0100

    fixes

--- screensaver.js
+++ screensaver.js
@@ -9,9 +9,9 @@
       el.style.position = "fixed";
 
       el.className = "Screensaver";
-      el.innerHTML = `<div>${(currentScript &&
+      el.innerHTML = `<div><div>${(currentScript &&
         currentScript.getAttribute("message")) ||
-        window.location.hostname}</div>`;
+        window.location.hostname}</div></div>`;
 
       const width = el.offsetWidth;
       const height = el.offsetHeight;
@@ -22,20 +22,18 @@
 #${id} {
   left: 0; top: 0; right: 0; bottom: 0;
   z-index: 100000;
-  background-color: #000;
   color: #eee;
-  box-shadow: 0 0 0 100000px #000;
+  background-color: #000;
 }
-#${id},
 #${id} div {
   width: ${width}px;
   height: ${height}px;
   line-height: 1;
 }
-#${id} {
+#${id} > div {
   animation: x${id} 13s linear infinite alternate;
 }
-#${id} div {
+#${id} > div > div {
   animation: y${id} 7s linear infinite alternate;
 }
 @keyframes x${id} {
